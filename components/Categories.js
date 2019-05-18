3e263bbf2dabe7e96b17f06464abbff12c9942a4
Anton Zimnitski <zimniros@protonmail.com>
2019-05-18 14:40:36 +0300

    Add pagination for categories. Add styles for buttons.

--- components/Categories.js
+++ components/Categories.js
@@ -4,35 +4,85 @@ import gql from 'graphql-tag';
 
 import Category from './Category';
 
-const ALL_CATEGORIES_QUERY = gql`
-  query ALL_CATEGORIES_QUERY {
-    categories {
-      id
-      itunesId
-      name
+const CATEGORIES_CONNECTION_QUERY = gql`
+  query CATEGORIES_CONNECTION_QUERY($after: String) {
+    categoriesConnection(first: 15, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+
+      edges {
+        node {
+          id
+          name
+          itunesId
+        }
+      }
     }
   }
 `;
 
 class Categories extends Component {
+  state = {
+    isLoading: false,
+  };
+
   render() {
     return (
       <div className="categories">
         <h3 className="categories__sub-title">All Categories</h3>
-        <Query query={ALL_CATEGORIES_QUERY}>
-          {({ data, error, loading }) => {
+        <Query query={CATEGORIES_CONNECTION_QUERY}>
+          {({ data, error, loading, fetchMore }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
 
-            const { categories } = data;
+            const { isLoading } = this.state;
+            const { categoriesConnection } = data;
+            const { edges, pageInfo } = categoriesConnection;
+            const { hasNextPage, endCursor } = pageInfo;
 
             return (
-              <div>
-                {categories &&
-                  categories.map(category => (
-                    <Category key={category.id} category={category} />
-                  ))}
-              </div>
+              <>
+                <div className="categories__list">
+                  {edges &&
+                    edges.map(({ node }) => (
+                      <Category key={node.id} category={node} />
+                    ))}
+                </div>
+                {hasNextPage ? (
+                  <button
+                    type="button"
+                    aria-disabled={isLoading}
+                    disabled={isLoading}
+                    className="btn btn--large"
+                    onClick={() => {
+                      this.setState({ isLoading: true });
+                      fetchMore({
+                        variables: { after: endCursor },
+                        updateQuery: (previousResult, { fetchMoreResult }) => {
+                          this.setState({ isLoading: false });
+                          if (!fetchMoreResult) return previousResult;
+
+                          return {
+                            categoriesConnection: {
+                              __typename: 'CategoriesConnection',
+                              pageInfo:
+                                fetchMoreResult.categoriesConnection.pageInfo,
+                              edges: [
+                                ...previousResult.categoriesConnection.edges,
+                                ...fetchMoreResult.categoriesConnection.edges,
+                              ],
+                            },
+                          };
+                        },
+                      });
+                    }}
+                  >
+                    Load more...
+                  </button>
+                ) : null}
+              </>
             );
           }}
         </Query>
