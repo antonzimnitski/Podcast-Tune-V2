dd020b744f986afdea9e94ffeb623654916705c5
Anton Zimnitski <zimniros@protonmail.com>
2019-08-31 17:35:56 +0300

    Refactor categories page using apollo hooks.

--- pages/categories.js
+++ pages/categories.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
 import CategoryItem from '../components/CategoryItem';
@@ -23,72 +23,62 @@ const CATEGORIES_CONNECTION_QUERY = gql`
   }
 `;
 
-class Categories extends Component {
-  state = {
-    isLoading: false,
-  };
+const Categories = () => {
+  const [isLoading, setLoadingStatus] = useState(false);
 
-  render() {
-    return (
-      <div className="categories container">
-        <h3 className="categories__sub-title">All Categories</h3>
-        <Query query={CATEGORIES_CONNECTION_QUERY}>
-          {({ data, error, loading, fetchMore }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error: {error.message}</p>;
+  const { data, error, loading, fetchMore } = useQuery(
+    CATEGORIES_CONNECTION_QUERY
+  );
 
-            const { isLoading } = this.state;
-            const { categoriesConnection } = data;
-            const { edges, pageInfo } = categoriesConnection;
-            const { hasNextPage, endCursor } = pageInfo;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
-            return (
-              <>
-                <div className="categories__list">
-                  {edges &&
-                    edges.map(({ node }) => (
-                      <CategoryItem key={node.id} category={node} />
-                    ))}
-                </div>
-                {hasNextPage ? (
-                  <button
-                    type="button"
-                    aria-disabled={isLoading}
-                    disabled={isLoading}
-                    className="btn btn--large"
-                    onClick={() => {
-                      this.setState({ isLoading: true });
-                      fetchMore({
-                        variables: { after: endCursor },
-                        updateQuery: (previousResult, { fetchMoreResult }) => {
-                          this.setState({ isLoading: false });
-                          if (!fetchMoreResult) return previousResult;
+  const { categoriesConnection } = data;
+  const { edges, pageInfo } = categoriesConnection;
+  const { hasNextPage, endCursor } = pageInfo;
 
-                          return {
-                            categoriesConnection: {
-                              __typename: 'CategoriesConnection',
-                              pageInfo:
-                                fetchMoreResult.categoriesConnection.pageInfo,
-                              edges: [
-                                ...previousResult.categoriesConnection.edges,
-                                ...fetchMoreResult.categoriesConnection.edges,
-                              ],
-                            },
-                          };
-                        },
-                      });
-                    }}
-                  >
-                    Load more...
-                  </button>
-                ) : null}
-              </>
-            );
-          }}
-        </Query>
+  return (
+    <div className="categories container">
+      <h3 className="categories__sub-title">All Categories</h3>
+      <div className="categories__list">
+        {edges &&
+          edges.map(({ node }) => (
+            <CategoryItem key={node.id} category={node} />
+          ))}
       </div>
-    );
-  }
-}
+      {hasNextPage ? (
+        <button
+          type="button"
+          aria-disabled={isLoading}
+          disabled={isLoading}
+          className="btn btn--large"
+          onClick={() => {
+            setLoadingStatus(true);
+            fetchMore({
+              variables: { after: endCursor },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                setLoadingStatus(false);
+                if (!fetchMoreResult) return previousResult;
+
+                return {
+                  categoriesConnection: {
+                    __typename: 'CategoriesConnection',
+                    pageInfo: fetchMoreResult.categoriesConnection.pageInfo,
+                    edges: [
+                      ...previousResult.categoriesConnection.edges,
+                      ...fetchMoreResult.categoriesConnection.edges,
+                    ],
+                  },
+                };
+              },
+            });
+          }}
+        >
+          Load more...
+        </button>
+      ) : null}
+    </div>
+  );
+};
 
 export default Categories;
